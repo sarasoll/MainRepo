a048c329c4e488b1f129f7fbe2830328fdc27a2e
@sarasoll <jack-soll@live.com>
2022-02-05 22:21:27 +0300

    internet speed test

--- speedtst/main.py
+++ speedtst/main.py
@@ -1,5 +1,8 @@
 from turtle import speed
 import speedtest
 speed=speedtest.Speedtest()
-print(f'D speed= {speed.download()}')
-print(f'U speed= {speed.upload()}')
+print(f'Down speed= {speed.download()}')
+print(f'Up speed= {speed.upload()}')
+
+
+
