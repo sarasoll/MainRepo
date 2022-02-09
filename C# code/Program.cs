48ed9bb70b42cb159883efbbe3e8f4e2fd520afe
@sarasoll <jack-soll@live.com>
2022-02-09 19:16:14 +0300

    new c# project

---
+++ C# code/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__code
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s="Hey Soll!";
+            Console.WriteLine(s);
+        }
+    }
+}
