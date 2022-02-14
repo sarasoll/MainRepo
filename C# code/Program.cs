aba39c5697661f7044e6400fbfd63e0ee188d051
@sarasoll <jack-soll@live.com>
2022-02-14 15:24:45 +0300

    ASP.NET website project

--- C# code/Program.cs
+++ C# code/Program.cs
@@ -7,19 +7,25 @@ namespace C__code
     {
         static void Main(string[] args)
         {
-            var age = 170;
-            if (age >= 90)
-            {
-                Console.WriteLine("Old is GOLD");
-            }
-            else if (age <= 99)
-            {
-                Console.WriteLine("too young");
-            }
-            else
-            {
-                Console.WriteLine("Just a BABY");
-            }
+int wssa=5;
+switch(wssa)
+{
+case 1:
+Console.WriteLine("W");
+break;
+case 2:
+Console.WriteLine("Sp");
+break;
+case 3:
+Console.WriteLine("Su");
+break;
+case 4:
+Console.WriteLine("A");
+break;
+default:
+Console.WriteLine("Skip >>>>");
+break;
+}
         }
     }
 }
---
+++ tempCodeRunnerFile.java
@@ -0,0 +1,8 @@
+package com.amigoscode;
+public class Main
+{
+    public public static void Main(String[] args)
+    {
+        System.out.println("Hey Soll");
+    }
+}
