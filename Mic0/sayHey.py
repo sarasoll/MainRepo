8d685aea7a7782e00ad2b43d4eae951fd02b1dff
sarasoll <solllsara@gmail.com>
2022-03-26 16:10:54 +0300

    Say Hey to AI ;)

---
+++ Mic0/sayHey.py
@@ -0,0 +1,15 @@
+import re
+import speech_recognition
+
+recognizer = speech_recognition.Recognizer()
+with speech_recognition.Microphone() as source:
+    print("Say something:")
+audio = recognizer.listen(source)
+
+words = recognizer.recognize_google(audio)
+
+matches = re.search("my name is (.*)", words)
+if matches:
+    print(f"Hey, {matches[1]}.")
+else:
+    print("Hey, you.")
