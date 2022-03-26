849f05b267985bd7f28ec8819013eceffd6cf012
sarasoll <solllsara@gmail.com>
2022-03-26 16:01:42 +0300

    new SayHey

---
+++ HeyWithYourName/sayhey.py
@@ -0,0 +1,11 @@
+words = input("say Hey!\n").lower()
+
+if "hey" in words:
+    print ("Heeeeyyyyyy!")
+elif "ummm" in words:
+    print ("Nice")
+elif "Bye!" in words:
+    print ("Biiiii!")
+else:
+    print ("Whattt!!!!")
+
