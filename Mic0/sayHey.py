27103ed87eccd709ad7eea8a63f89545daa67c65
sarasoll <solllsara@gmail.com>
2022-03-26 16:29:57 +0300

    A.I Saying Hello to... ;)

--- Mic0/sayHey.py
+++ Mic0/sayHey.py
@@ -4,7 +4,7 @@ import speech_recognition
 recognizer = speech_recognition.Recognizer()
 with speech_recognition.Microphone() as source:
     print("Say something:")
-audio = recognizer.listen(source)
+    audio = recognizer.listen(source)
 
 words = recognizer.recognize_google(audio)
 
