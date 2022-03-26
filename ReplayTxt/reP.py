5007b9f3f0a8ff31049dc13c36e7ed73bfb9db61
sarasoll <solllsara@gmail.com>
2022-03-26 15:57:34 +0300

    ReplayTxt

---
+++ ReplayTxt/reP.py
@@ -0,0 +1,10 @@
+w = input("I am all ears ; )").lower()
+
+if "hey" in w:
+    print("impresive!")
+elif "hi" in w:
+    print("wow")
+elif "bye" in w:
+    print("nice")
+else:
+    print("hummmmmm!")
